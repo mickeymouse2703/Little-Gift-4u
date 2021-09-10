a1860c85aa47b53e1c9b06caf8270f011ae425b1
mickeymouse2703 <90429459+mickeymouse2703@users.noreply.github.com>
2021-09-10 09:44:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     introTitle: 'Hallo cậu!',
     introDesc: `từ lần đầu nhìn thấy cậu
     Tớ đã bị ấn tượng nhiều lắm, tiếc là
-    Chỉ nhìn được cậu qua fb và học online.
+    Chỉ nhìn được cậu qua fb và học online thui.
     Không sao, vì nhờ đó tớ đã làm ra món quà này.`,
     btnIntro: 'Let go',
     title: 'Nếu lỡ mai đây vô tình thấy được nhau😊😊',
     desc: 'Hãy để cho tớ nói với cậu lời chào😆😆',
     btnYes: 'Awwww :>>>',
     btnNo: 'Không,mình không có hứng🙃🙃',
-    question:'Cảm nhận của cậu về món quà nhỏ của tớ',
+    question:'Nếu trái tim ta chung nỗi nhớ đong đầy...',
     btnReply: 'Gửi cho tớ :>>',
-    reply: 'Tớ thích nó lắm, Cảm ơn cậu :>>',
-    mess: 'Tớ biết mà 🥰.món quà này là để...',
-    messDesc: 'Thay tớ làm quen với cậu, có được khum:>>',
-    btnAccept: 'Okiii lunn :>>',
+    reply: 'Hẹn gặp lại cậu ngày tháng của sau này :>>>',
+    mess: 'Tuỵt🥰.món quà nhỏ này là để...',
+    messDesc: 'Thay tớ làm quen với cậu đó, có được khum:>>',
+    btnAccept: 'Okiii lunn :>>>',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
