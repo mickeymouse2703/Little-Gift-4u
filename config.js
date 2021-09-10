6932039e829973937a58826102885952f2b42503
mickeymouse2703 <90429459+mickeymouse2703@users.noreply.github.com>
2021-09-10 08:31:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introDesc: `từ lần đầu nhìn thấy cậu
     Tớ đã bị ấn tượng nhiều lắm, tiếc là
     Chỉ nhìn được cậu qua fb và học online.
-    Không sao, vì nhờ đó tớ đã làm ra món quà này`,
-    btnIntro: 'let go',
-    title: 'Này người tớ luôn tìm kiếm lâu nay...',
-    desc: 'Trễ bao năm rồi đấy😉😉 ',
+    Không sao, vì nhờ đó tớ đã làm ra món quà này.`,
+    btnIntro: 'Let go',
+    title: 'Nếu lỡ mai đây vô tình thấy được nhau😊😊',
+    desc: 'Hãy để cho tớ nói với cậu lời chào😆😆',
     btnYes: 'Awwww :>>>',
     btnNo: 'Không,mình không có hứng🙃🙃',
-    question:'cậu thấy món quà nhỏ này như thế nào',
+    question:'Cảm nhận của cậu về món quà nhỏ của tớ',
     btnReply: 'Gửi cho tớ :>>',
-    reply: 'Tớ thích nó lắm <333',
+    reply: 'Tớ thích nó lắm, Cảm ơn cậu :>>',
     mess: 'Tớ biết mà 🥰.món quà này là để...',
     messDesc: 'Thay tớ làm quen với cậu, có được khum:>>',
     btnAccept: 'Okiii lunn :>>',
