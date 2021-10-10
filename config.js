3697699033f3206ede0868e0c3676bc6657c710a
mickeymouse2703 <90429459+mickeymouse2703@users.noreply.github.com>
2021-10-10 19:01:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'đệ tử iu !',
+    introTitle: 'đệ tử iu ơiii !',
     introDesc: `có cái này hay lắm nè
-    đắc biệt lắm mới cho coi đó nha
+    đặc biệt lắm mới cho coi đó nha
     riêng đệ tử thuii đó 😉😉.`,
     btnIntro: 'Let go',
-    title: 'Phải chăng t đã yêu...nhưng hông phải từ cái nhìn đầu tiên😄😄',
+    title: 'Phải chăng t đã yêu...sui ghê,hông phải từ cái nhìn đầu tiên😢😢',
     desc: 'Phải chăng t đã say ngay từ lúc thấy nụ cười của m đó',
     btnYes: 'Awwww,xin củm ơn:>>>',
     btnNo: 'VL, xàm quá ba 🙃🙃',
     question:'Mong một ngày,m sẽ nhận ra sự cố gắng của t nha <3',
     btnReply: 'Send message...',
-    reply: ' Chắc chắn rùi, t cũng đang chờ coi m cố gắng như thế nào đók :>>>',
-    mess: ' cảm ơn nha🥰 M như ngày tháng của hôm nay vậy🥰',
+    reply: ' Yah, t cũng đang chờ coi m cố gắng như thế nào đók😉 :>>>',
+    mess: ' cảm ơn nha🥰 Trâm như ngày tháng của hôm nay vậy🥰',
     messDesc: 'Lúc nào cũng 10/10 hết❤️:>>',
-    btnAccept: 'Aww,cảm ơn nha :>',
+    btnAccept: 'Aww,cảm ơn ạk :>',
     messLink: 'https://www.messenger.com/t/100068628549994' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
